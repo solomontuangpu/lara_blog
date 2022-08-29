c65cf9201d9216fed01a33d318d730c64b281f1b
solomontuangpu <solomontuangpu@gmail.com>
2022-08-29 19:37:53 +0630

    seeder change

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
@@ -14,6 +17,13 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = ['Cele News', 'Sport News', "IT News", "Car News"];
+        foreach($categories as $category) {
+            Category::factory()->create([
+                "title" => $category,
+                "slug" => Str::slug($category),
+                "user_id" => User::inRandomOrder()->first()->id
+            ]);
+        }
     }
 }
